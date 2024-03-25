736baef3254ac5c099a746374a68c5793eeef343
hekzayas <hector.zay@gmail.com>
2024-03-24 19:51:40 -0600

    added scss styling

--- script.js
+++ script.js
@@ -4,7 +4,7 @@
 setTimeout(() => {
     document.querySelectorAll('.skeleton').forEach((el) => el.style.display = 'none');
     document.querySelectorAll('.hidden').forEach((el) => el.style.display = 'block');
-}, 3000);
+}, 1000);
 
 // Function to check if page is scrolled and adjust the logo size
 function checkScroll() {
@@ -31,3 +31,63 @@ function checkScroll() {
 
 // Event Listener to check scroll position
 window.addEventListener('scroll', checkScroll);
+
+
+// -------------------------------------------------
+// ------------------- Dark Mode -------------------
+// -------------------------------------------------
+const themeSwitcher = document.querySelector('.theme-switcher');
+
+// Update Theme Icon
+function darklightMode(newMode) {
+    const currentFavicon = newMode === 'dark' ? 'fa-sun' : 'fa-moon';
+    const newFavicon = newMode === 'dark' ? 'fa-moon' : 'fa-sun';
+    const capitalized = newMode.charAt(0).toUpperCase() + newMode.slice(1)
+
+    themeSwitcher.children[0].classList.replace(`${currentFavicon}`, `${newFavicon}`);
+}
+
+// Determines if Dark Mode is Enabled
+function prefersDarkScheme() {
+    return window.matchMedia && window.matchMedia('(prefers-color-scheme: dark)').matches;
+}
+
+// Set Initial Theme
+function setThemeBasedOnPreferences() {
+    const isDarkMode = prefersDarkScheme();
+    document.documentElement.setAttribute('data-theme', isDarkMode ? 'dark' : 'light');
+    const currentTheme = document.documentElement.getAttribute('data-theme');
+    darklightMode(currentTheme);
+}
+
+// Switch Theme
+function switchTheme() {
+    const currentTheme = document.documentElement.getAttribute('data-theme');
+    const themeMode = currentTheme === 'dark' ? 'light' : 'dark';
+
+    document.documentElement.setAttribute('data-theme', `${themeMode}`)
+    localStorage.setItem('theme', `${themeMode}`);
+    darklightMode(`${themeMode}`);
+}
+
+themeSwitcher.addEventListener('click', switchTheme);
+
+// Check local Storage for Theme
+function initializeTheme() {
+    const savedTheme = localStorage.getItem('theme');
+    if (savedTheme) {
+        document.documentElement.setAttribute('data-theme', savedTheme);
+        darklightMode(savedTheme);
+    } else {
+        setThemeBasedOnPreferences();
+    }
+}
+
+// Listen for system theme changes
+window.matchMedia('(prefers-color-scheme: dark)').addEventListener('change', setThemeBasedOnPreferences);
+
+// Initialize Theme when the script loads
+initializeTheme();
+// -------------------------------------------------
+// -------------------------------------------------
+// -------------------------------------------------
